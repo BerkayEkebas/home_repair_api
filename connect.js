f148c1fa16ae3099ded1309c1a1f2d488376bd9e
BerkayEkebas <48918056+BerkayEkebas@users.noreply.github.com>
2025-10-21 18:35:19 +0900

    add db

--- connect.js
+++ connect.js
@@ -1,8 +1,11 @@
 import mysql from 'mysql'
 
-export const db = mysql.createConnection({
-    host:"bpasfgzdepj5tujg2mws-mysql.services.clever-cloud.com",
-    user:"upl1pwizkddw176s",
-    password:"fofbfANvdp89zbksRxf1",
-    database:"bpasfgzdepj5tujg2mws"
-})
+export const db = mysql.createPool({
+  host: "bpasfgzdepj5tujg2mws-mysql.services.clever-cloud.com",
+  user: "upl1pwizkddw176s",
+  password: "fofbfANvdp89zbksRxf1",
+  database: "bpasfgzdepj5tujg2mws",
+  waitForConnections: true, 
+  connectionLimit: 10,      
+  queueLimit: 0,            
+});
