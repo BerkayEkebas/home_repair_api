f62fc0b1677d567ab70d4b60dd1c4fe7e11f50f0
BerkayEkebas <ekebasahmet@gmail.com>
2025-11-13 11:38:55 +0900

    Update getValues.js

--- controllers/getValues.js
+++ controllers/getValues.js
@@ -20,6 +20,7 @@ export const postRoomStatusTest = (req, res) => {
 
 export const postRoomStatus = (req, res) => {
   const { room_number, temperature, humidity, power_consumption, ac_status, window_status, room_occupancy } = req.body;
+  console.log(req.body);
 
   if (!room_number) {
     return res.status(400).json({ error: "room_number zorunlu" });
